d175dc7b71bd858cac0886511d8b39cf521b9c77
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-09-07 14:04:36 -0600

    Add HTTP Basic Authentication for admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,17 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authenticate_admin
+
   def show
+    @total_products = Product.count
+    @total_orders = Order.count
+    @total_revenue = Order.sum(:total_cents) / 100.0 # assuming total_cents is in cents
+  end
+
+  private
+
+  def authenticate_admin
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
   end
 end
