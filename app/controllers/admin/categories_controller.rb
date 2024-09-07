6afc7fea62c4093be3b0e5dc5fa1e78d3c584ccc
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-09-07 15:23:30 -0600

    Add navigation links for admin categories management

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -5,7 +5,24 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_path, notice: "Category was successfully created"
+    else
+      render :new
+    end
+  end
+
   private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 
   def authenticate_admin
     authenticate_or_request_with_http_basic do |username, password|
