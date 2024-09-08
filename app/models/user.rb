297551a662973b5af1921f05392d4ae1fcfd41ec
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-09-07 20:20:35 -0600

    Add user authentication with signup and login functionality

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
+
+  private
+
+  def password_required?
+    password_digest.nil? || !password.blank?
+  end
+end
